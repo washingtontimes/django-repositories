193e0c70a85fb832ac04b77543bce7179c24129f
Corey Oordt <coordt@washingtontimes.com>
2009-11-27 19:52:49 -0500

    Reconfigured how the apache configuration is exported. It was duplicating the authentication settings and apache didn't like it.

--- repositories/management/commands/export_apache_conf.py
+++ repositories/management/commands/export_apache_conf.py
@@ -1,20 +1,31 @@
 import os
-from django.core.management.base import LabelCommand, CommandError
+from django.core.management.base import BaseCommand, CommandError
 from django.template import Template, Context
 from django.template.loader import get_template
 
 from repositories import settings
 
-class Command(LabelCommand):
+class Command(BaseCommand):
     def __init__(self):
         super(Command, self).__init__()
         VCS = {}
         for key, value in settings.VCS_CONFIG.items():
             VCS[value['name'].lower()] = value
         self.VCS = VCS
-        print VCS
     
-    def handle_label(self, label, **options):
+    def handle(self, *args, **options):
+        tmpl = get_template('repositories/apache2_auth.conf')
+        ctxt_dict = {}
+        ctxt_dict['wsgi_auth_script_path'] = os.path.dirname(settings.WSGI_AUTH_SCRIPT)
+        ctxt_dict['wsgi_auth_script'] = settings.WSGI_AUTH_SCRIPT
+        ctxt_dict['wsgi_script'] = settings.WSGI_SCRIPT
+        ctxt = Context(ctxt_dict)
+        print tmpl.render(ctxt)
+        
+        for key in self.VCS.keys():
+            self.handle_type(key)
+    
+    def handle_type(self, label, **options):
         try:
             vcs_config = self.VCS[label]
         except KeyError:
