6d4264b5714c01a3ba934cd48eb9419cc5917ad5
Corey Oordt <coordt@washingtontimes.com>
2009-11-27 19:09:48 -0500

    Added the content of the setup.py file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='django-repositories',
+      version='0.1',
+      description='Version Control system management',
+      long_description='',
+      author='Corey Oordt',
+      author_email='coordt@washingtontimes.com',
+      url='http://opensource.washingtontimes.com/projects/django-repositories/',
+      packages=['repositories'],
+      classifiers=['Development Status :: 3 - Alpha',
+          'Framework :: Django',
+          'License :: OSI Approved :: Apache Software License',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development',
+          ],
+      )
