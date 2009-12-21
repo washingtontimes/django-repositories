88a62a18913199e4a3d5eb1ea5f26b61c246e948
Corey Oordt <coordt@washingtontimes.com>
2009-12-21 13:35:33 -0500

    Modified the setup.py to include the directories below the repositories. Using distribute and needed a MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
-from distutils.core import setup
+from distribute_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup
 
 setup(name='django-repositories',
-      version='0.1',
+      version='0.1.1',
       description='Version Control system management',
       long_description='',
+      license='Apache Software License',
       author='Corey Oordt',
       author_email='coordt@washingtontimes.com',
       url='http://opensource.washingtontimes.com/projects/django-repositories/',
-      packages=['repositories'],
+      packages=['repositories',],
+      package_data={
+          'repositories': ['repositories/templates/repositories/*.conf'] #['bin/*.sh', 'management/*', 'repo-templates/*', 'templates/repositories/*.conf']
+      },
       classifiers=['Development Status :: 3 - Alpha',
           'Framework :: Django',
           'License :: OSI Approved :: Apache Software License',
