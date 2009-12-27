938d65014b09bad4ed6b97b2a6e6428f3cc811a3
Corey Oordt <coordt@washingtontimes.com>
2009-12-27 14:54:34 -0500

    Updated the basic project files for a readme, apache license and a better setup.py file

--- setup.py
+++ setup.py
@@ -3,18 +3,21 @@ use_setuptools()
 
 from setuptools import setup
 
+try:
+    long_description = open('README').read()
+except IOError:
+    long_description = ''
+
 setup(name='django-repositories',
       version='0.1.1',
       description='Version Control system management',
-      long_description='',
+      long_description=long_description,
       license='Apache Software License',
       author='Corey Oordt',
       author_email='coordt@washingtontimes.com',
       url='http://opensource.washingtontimes.com/projects/django-repositories/',
       packages=['repositories',],
-      package_data={
-          'repositories': ['repositories/templates/repositories/*.conf'] #['bin/*.sh', 'management/*', 'repo-templates/*', 'templates/repositories/*.conf']
-      },
+      include_package_data=True,
       classifiers=['Development Status :: 3 - Alpha',
           'Framework :: Django',
           'License :: OSI Approved :: Apache Software License',
